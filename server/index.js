7a6b8f4b041e4421b09e91e99e3928fcf8007cf9
soumyaarya <iec2023017@iiita.ac.in>
2026-01-17 09:23:01 +0530

    Fix: Express 5 wildcard syntax for production

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '..', 'dist')));
 
     // Handle React routing - send all non-API requests to index.html
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
     });
 } else {
